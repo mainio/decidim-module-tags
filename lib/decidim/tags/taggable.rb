eaf13b3764072e2de39d1576a0e18da45feecc0a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-07-01 17:42:48 +0300

    Add `with_any_tag` scope to the taggable for filtering

--- lib/decidim/tags/taggable.rb
+++ lib/decidim/tags/taggable.rb
@@ -14,6 +14,35 @@ module Decidim
                  class_name: "Decidim::Tags::Tagging",
                  dependent: :destroy
         has_many :tags, through: :taggings
+
+        # This scope can be used as a ransack filter. To make it possible to
+        # filter using this scope, you also need to add the following to your
+        # model (note that you may also want to add some other scopes):
+        #
+        #   def self.ransackable_scopes(_auth_object = nil)
+        #     [:with_any_tag]
+        #   end
+        #
+        # After this, you are able to filter within your controllers using this
+        # scope by adding the following to the controller performing the search
+        # (note that you may also want to include some other filters):
+        #
+        #  def default_filter_params
+        #    { with_any_tag: [] }
+        #  end
+        scope :with_any_tag, lambda { |*tags|
+          provided_tags = tags.compact_blank
+          return self if provided_tags.empty?
+
+          # Fetch the record IDs as a separate query in order to avoid duplicate
+          # entries in the final result. We could also use `.distinct` on the
+          # main query but that would limit what the user could further on do
+          # with that query. Therefore, in this context it is safer to just
+          # fetch these in a completely separate query.
+          record_ids = joins(:tags).where(decidim_tags_tags: { id: provided_tags }).distinct.pluck(:id)
+
+          where(id: record_ids)
+        }
       end
     end
   end
