b89c3d903bd4481578862d47c904e1f4f25827bb
Joonas <joonas.aapro@mainiotech.fi>
2026-08-06 09:32:07 +0300

    Upgrade to v0.31

--- config/assets.rb
+++ config/assets.rb
@@ -2,12 +2,12 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs", prepend: true)
+Decidim::Shakapacker.register_path("#{base_path}/app/packs", prepend: true)
 
-Decidim::Webpacker.register_entrypoints(
+Decidim::Shakapacker.register_entrypoints(
   decidim_tags: "#{base_path}/app/packs/entrypoints/decidim_tags.js",
   decidim_tags_admin: "#{base_path}/app/packs/entrypoints/decidim_tags_admin.js"
 )
 
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/tags/tagging-input")
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/tags/tagging-input", group: :admin)
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/tags/tagging-input")
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/tags/tagging-input", group: :admin)
